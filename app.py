ae5309e775b6e439634b8af44727072d467e9755
Arya Y <137373773+Aryasree20@users.noreply.github.com>
2026-03-09 15:47:32 +0530

    done

--- app.py
+++ app.py
@@ -48,8 +48,6 @@ def index():
             return "Something went wrong"
     else:
         return render_template('index.html')
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
     
